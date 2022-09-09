0b9014d1793f9fc7b2dcb02e2d69c64826a404b3
jacky32 <patrki@atlas.cz>
2022-09-09 20:59:48 +0200

    Add app logic, project and todo classes

--- src/index.js
+++ src/index.js
@@ -2,41 +2,6 @@ import "./initial.css";
 import "./main.css";
 import "./header.css";
 
-// <header>
-// <div id="burger-icon">=</div>
-// <div id="add-todo">+</div>
-// <div id="settings">x</div>
-// </header>
-// <div id="main">
-// <div id="project-content">
-//   <input
-//     type="text"
-//     name="New project name"
-//     id="new-project"
-//     placeholder="New project name"
-//   />
-//   <div id="add-project">+</div>
-//   <div id="project-list">
-//     <div class="project">
-//       <div class="project-name">Aaa</div>
-//       <div class="project-rename">R</div>
-//       <div class="project-delete">X</div>
-//     </div>
-//   </div>
-// </div>
-// <div id="todo-content">
-//   <div class="todo">
-//     <div class="todo-name">Name</div>
-//     <div class="todo-rename">R</div>
-//     <div class="todo-delete">X</div>
-//     <div class="todo-due-date">12/12/2020</div>
-//     <div class="todo-edit">Edit</div>
-//     <div class="todo-description">Description</div>
-//     <div class="todo-star">*</div>
-//   </div>
-// </div>
-// </div>
-
 class Builder {
   constructor() {
     this.content = document.getElementById("content");
@@ -51,6 +16,10 @@ class Builder {
     burgerIcon.id = "burger-icon";
     addTodo.id = "add-todo";
     settings.id = "settings";
+
+    burgerIcon.textContent = "E";
+    addTodo.textContent = "+";
+    settings.textContent = "*";
 
     header.append(burgerIcon, addTodo, settings);
     content.appendChild(header);
@@ -73,11 +42,15 @@ class Builder {
     addProject.id = "add-project";
     this.projectList.id = "project-list";
 
+    addProject.addEventListener("click", () => {
+      App.createProject(newProjectText.value);
+    });
+
     projectContent.append(newProjectText, addProject, this.projectList);
-    main.appendChild(projectContent);
+    this.main.appendChild(projectContent);
   }
 
-  static buildProject(name) {
+  static buildProject(id, name) {
     const project = document.createElement("div");
     const projectName = document.createElement("div");
     const projectRename = document.createElement("div");
@@ -92,12 +65,133 @@ class Builder {
     projectRename.innerText = "R";
     projectDelete.innerText = "X";
 
+    projectRename.addEventListener("click", () => {
+      console.log("rename");
+    });
+
+    projectDelete.addEventListener("click", () => {
+      App.deleteProject(id);
+      project.remove();
+    });
+
     project.append(projectName, projectRename, projectDelete);
     this.projectList.appendChild(project);
   }
+
+  static buildTodoContent() {
+    this.todoContent = document.createElement("div");
+    this.todoContent.id = "todo-content";
+
+    this.main.appendChild(this.todoContent);
+  }
+
+  static buildTodo(id, name, dueDate, description) {
+    const todo = document.createElement("div");
+    const todoName = document.createElement("div");
+    const todoRename = document.createElement("div");
+    const todoDelete = document.createElement("div");
+    const todoDueDate = document.createElement("div");
+    const todoEdit = document.createElement("div");
+    const todoDescription = document.createElement("div");
+    const todoStar = document.createElement("div");
+
+    todo.classList = "todo";
+    todoName.classList = "todo-name";
+    todoRename.classList = "todo-rename";
+    todoDelete.classList = "todo-delete";
+    todoDueDate.classList = "todo-due-date";
+    todoEdit.classList = "todo-edit";
+    todoDescription.classList = "todo-description";
+    todoStar.classList = "todo-star";
+
+    todoName.innerText = "The name";
+    todoRename.innerText = "R";
+    todoDelete.innerText = "X";
+    todoDueDate.innerText = "12/12/1222";
+    todoEdit.innerText = "Edit";
+    todoDescription.innerText = "Description";
+    todoStar.innerText = "*";
+
+    todo.append(
+      todoName,
+      todoRename,
+      todoDelete,
+      todoDueDate,
+      todoEdit,
+      todoDescription,
+      todoStar
+    );
+    this.todoContent.appendChild(todo);
+  }
 }
 
-Builder.buildHeader();
-Builder.buildMain();
-Builder.buildProjectList();
-Builder.buildProject("projectname");
+class App {
+  static projectCount = 0;
+  static projects = {};
+  constructor() {
+    this.#buildAll();
+  }
+
+  #buildAll() {
+    Builder.buildHeader();
+    Builder.buildMain();
+    Builder.buildProjectList();
+    Builder.buildTodoContent();
+  }
+
+  static createProject(name) {
+    const id = this.projectCount;
+    const project = new Project(id, name);
+    this.projects[id] = project;
+    this.projectCount++;
+  }
+
+  static deleteProject(id) {
+    delete this.projects[id];
+    console.log(this.projects);
+  }
+}
+
+class Project {
+  constructor(id, name) {
+    this.todoCount = 0;
+    this.todos = {};
+    this.id = id;
+    this.name = name;
+    Builder.buildProject(this.id, this.name);
+  }
+
+  get getId() {
+    return this.id;
+  }
+
+  createTodo(name, dueDate, description) {
+    const todoId = this.todoCount;
+    const todo = new Todo(todoId, name, dueDate, description);
+    this.todos.todoId = todo;
+    this.todoCount++;
+  }
+}
+
+class Todo {
+  constructor(
+    id,
+    name = "Default name",
+    dueDate = "12/12/1212",
+    description = "Default description"
+  ) {
+    this.id = id;
+    this.name = name;
+    this.dueDate = dueDate;
+    this.description = description;
+    Builder.buildTodo(this.id, this.name, this.dueDate, this.description);
+  }
+
+  get getId() {
+    return this.id;
+  }
+}
+
+const app = new App();
+App.createProject("jmeno");
+console.log(app.projects);
